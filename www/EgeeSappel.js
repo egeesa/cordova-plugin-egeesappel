e43d626aa466bfa86f4b547caee7353b3f4d9ba1
EGEE\eca <emilie.carreras@egee.fr>
2020-08-21 16:56:58 +0200

    ligne ajoutait lors du plugin add automatiquement

--- www/EgeeSappel.js
+++ www/EgeeSappel.js
@@ -1,51 +1,50 @@
-cordova.define("cordova.plugin.egeesappel.EgeeSappel", function(require, exports, module) {
-    var exec = require('cordova/exec');
-    
-    module.exports.getVersion = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'getVersion', []);
-    };
+var exec = require('cordova/exec');
 
-    module.exports.readDeviceConfiguration = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'readDeviceConfiguration', []);
-    };
+module.exports.getVersion = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'getVersion', []);
+};
 
-    module.exports.openConfigurationConnection = function (arg0,success, error) {
-        exec(success, error, 'EgeeSappel', 'openConfigurationConnection', [arg0]);
-    };
+module.exports.readDeviceConfiguration = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'readDeviceConfiguration', []);
+};
 
-    module.exports.initializeConfiguration = function (arg0,success, error) {
-        exec(success, error, 'EgeeSappel', 'initializeConfiguration', [arg0]);
-    };
+module.exports.openConfigurationConnection = function (arg0,success, error) {
+	exec(success, error, 'EgeeSappel', 'openConfigurationConnection', [arg0]);
+};
 
-    module.exports.closeConfigConnection = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'closeConfigConnection', []);
-    };
+module.exports.initializeConfiguration = function (arg0,success, error) {
+	exec(success, error, 'EgeeSappel', 'initializeConfiguration', [arg0]);
+};
 
-    module.exports.setDeviceConfiguration = function (arg0,success, error) {
-        exec(success, error, 'EgeeSappel', 'setDeviceConfiguration', [arg0]);
-    };
+module.exports.closeConfigConnection = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'closeConfigConnection', []);
+};
 
-    module.exports.openConnection = function (arg0, success, error) {
-        exec(success, error, 'EgeeSappel', 'openConnection', [arg0]);
-    };
+module.exports.setDeviceConfiguration = function (arg0,success, error) {
+	exec(success, error, 'EgeeSappel', 'setDeviceConfiguration', [arg0]);
+};
 
-    module.exports.pollFrames = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'pollFrames', []);
-    };
+module.exports.openConnection = function (arg0, success, error) {
+	exec(success, error, 'EgeeSappel', 'openConnection', [arg0]);
+};
 
-    module.exports.closeConnection = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'closeConnection', []);
-    };
+module.exports.pollFrames = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'pollFrames', []);
+};
 
-    module.exports.openConnectionStatic = function (arg0, success, error) {
-        exec(success, error, 'EgeeSappel', 'openConnectionStatic', [arg0]);
-    };
+module.exports.closeConnection = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'closeConnection', []);
+};
 
-    module.exports.pollFrameStatic = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'pollFrameStatic', []);
-    };
+module.exports.openConnectionStatic = function (arg0, success, error) {
+	exec(success, error, 'EgeeSappel', 'openConnectionStatic', [arg0]);
+};
 
-    module.exports.closeConnectionStatic = function (success, error) {
-        exec(success, error, 'EgeeSappel', 'closeConnectionStatic', []);
-    };
-});
+module.exports.pollFrameStatic = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'pollFrameStatic', []);
+};
+
+module.exports.closeConnectionStatic = function (success, error) {
+	exec(success, error, 'EgeeSappel', 'closeConnectionStatic', []);
+};
+
